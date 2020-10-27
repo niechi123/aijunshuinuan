9198edbc41c3a0c5ee24ffe1696b22e165db30f1
niechi123 <nycqwert@163.com>
2020-10-27 17:53:29 +0800

    调整了页面

--- miniprogram/pages/manage/addshop/addshop.js
+++ miniprogram/pages/manage/addshop/addshop.js
@@ -8,9 +8,24 @@ Page({
 
   data: {
     pickerHidden: true,
-    chosen: ''
+    chosen: '',
+    show:false,//控制下拉列表的显示隐藏，false隐藏、true显示
+    selectData:['1','2','3','4','5','6'],//下拉列表的数据
+    index:0//选择的下拉列表下标
   },
-
+  selectTap(){
+    this.setData({
+     show: !this.data.show
+    });
+    },
+    // 点击下拉列表
+    optionTap(e){
+      let Index=e.currentTarget.dataset.index;//获取点击的下拉列表的下标
+      this.setData({
+      index:Index,
+      show:!this.data.show
+      });
+    },
   pickerConfirm(e) {
     this.setData({
       pickerHidden: true
